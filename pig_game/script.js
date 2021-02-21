8ffba5adda6fd576295db9a88c076c480086c3e4
Codeorion <codeorion12@gmail.com>
2021-02-21 17:40:41 +0200

    dice game

---
+++ pig_game/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const score0EL = document.getElementById("score--0");
+const score1EL = document.getElementById("score--1");
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+const diceEl = document.querySelector(".dice");
+diceEl.classList.add("hidden");
