5b463d728f560ff2b5f67cb9988633425c8e670f
Codeorion <codeorion12@gmail.com>
2021-02-21 10:57:50 +0200

    finish game

--- gues_a_number/script.js
+++ gues_a_number/script.js
@@ -3,6 +3,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -13,6 +14,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "Correct Number";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".score").textContent = score;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector(".message").textContent = "Too High";
