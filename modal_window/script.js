e89e0e733321630e482cc47377ef568f36624093
Codeorion <codeorion12@gmail.com>
2021-02-21 15:06:14 +0200

    finish modal window

--- modal_window/script.js
+++ modal_window/script.js
@@ -4,7 +4,18 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const OpenModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden"); // видалення прихованого класу для відображення
+  overlay.classList.remove("hidden"); // відображення розмиття на задньому фоні
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener("click", OpenModal);
+  btnCloseModal.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
 }
