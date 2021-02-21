ebb7c00a2c08558c8a8ba36235865a31a0c3991b
Codeorion <codeorion12@gmail.com>
2021-02-21 11:57:08 +0200

    refactoring

--- gues_a_number/script.js
+++ gues_a_number/script.js
@@ -5,13 +5,16 @@ document.querySelector(".number").textContent = secretNumber;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
   if (!guess) {
-    document.querySelector(".message").textContent = "No number";
+    displayMessage("No number");
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Correct Number";
+    displayMessage("Correct Number");
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".score").textContent = score;
@@ -19,22 +22,13 @@ document.querySelector(".check").addEventListener("click", function () {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
     }
-  } else if (guess > secretNumber) {
-    if (score > 0) {
-      document.querySelector(".message").textContent = "Too High";
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "Too High" : "Too Low");
       score -= 1;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "Yoo lost the game";
-      document.querySelector(".score").textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 0) {
-      document.querySelector(".message").textContent = "Too Low";
-      score -= 1;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "Yoo lost the game";
+      displayMessage("Yoo lost the game");
       document.querySelector(".score").textContent = 0;
     }
   }
